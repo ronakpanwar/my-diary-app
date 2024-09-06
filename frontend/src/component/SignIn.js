1ec87b655a78e1c07ff8034a071b7cdaf9c1b43d
Ronak panwar <127338535+ronakpanwar@users.noreply.github.com>
2024-09-06 11:58:39 +0530

    Update SignIn.js

--- frontend/src/component/SignIn.js
+++ frontend/src/component/SignIn.js
@@ -25,7 +25,7 @@ function SignIn() {
     e.preventDefault();
    
     
-    const response = await fetch('http://localhost:4000/api/user/login', {
+    const response = await fetch('https://my-diary-app-api.vercel.app/api/user/login', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json'
