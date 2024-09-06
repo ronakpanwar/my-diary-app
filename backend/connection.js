64bba8ac273f5797df81a96af5aa320c29a7f06d
Ronak panwar <127338535+ronakpanwar@users.noreply.github.com>
2024-09-06 14:24:29 +0530

    Update connection.js

--- backend/connection.js
+++ backend/connection.js
@@ -1,7 +1,11 @@
 const mongoose = require('mongoose');
 
 async function connectMongoDb(url){
-    return await mongoose.connect(url);
+    return await mongoose.connect(url ,{
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    serverSelectionTimeoutMS: 5000, // Adjust the timeout as needed
+  });
 }
 
 module.exports = {
