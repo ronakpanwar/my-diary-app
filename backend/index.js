2fb3976e3b08084307565038523b7f4271bb8bc0
Ronak panwar <127338535+ronakpanwar@users.noreply.github.com>
2024-09-06 12:12:08 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -12,13 +12,11 @@ const URL = 'mongodb+srv://vilanrn:zEUwF5JQV87lkhcf@my-diary.75arg.mongodb.net/'
 
 
 const app = express();
-app.use(cors(
-    {
-        origin:["https://my-diary-app-frontend.vercel.app"],
-        methods:["POST","GET"],
-        credentials:true
-    }
-));
+app.use(cors({
+  origin: 'https://my-diary-app-frontend.vercel.app', // Replace with your frontend's URL
+  methods: ['GET', 'POST', 'PUT', 'DELETE'], // Define allowed HTTP methods if necessary
+  credentials: true // Enable this if you're sending cookies or authorization headers
+}));
 app.use(express.json());
 
 connectMongoDb(URL, { useNewUrlParser: true, useUnifiedTopology: true }).then(()=>console.log('mongoBb Connected')).catch((err)=> console.log("error",err));
